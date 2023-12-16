9775d5b898e3cc45041c1b268e9c32e55ee7fdee
dbleonardo <davidbl.1281@gmail.com>
2023-12-16 17:05:31 -0300

    Inicio do projeto.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descreva o projeto aqui.">
+    <meta name="keywords" content="coloque, aqui, as, palavras, separadas, por, virgula">
+    <meta name="author" content="David Berriel Leonardo">
+    <link rel="stylesheet" href="./style/bootstrap.min.css">
+    <title>Olha o que ele fez...</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <script src="./script/bootstrap.bundle.min.js"></script>
+</body>
+</html>
