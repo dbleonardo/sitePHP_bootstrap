a67dee10919c71b8c0bdb5a5c76daca4f4d3b290
dbleonardo <davidbl.1281@gmail.com>
2023-12-19 11:02:15 -0300

    :package: Feito o menu e o banner.

--- index.php
+++ index.php
@@ -1,37 +1,69 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Descreva o projeto aqui.">
-    <meta name="keywords" content="coloque, aqui, as, palavras, separadas, por, virgula">
-    <meta name="author" content="David Berriel Leonardo">
-    <link rel="stylesheet" href="./style/bootstrap.min.css">
-    <title>Olha o que ele fez...</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="descreva o documento aqui">
+  <meta name="keywords" content="coloque, palavras, chaves, entre, virgulas, aqui">
+  <link rel="stylesheet" href="./style/bootstrap.min.css">
+  <link rel="stylesheet" href="./style/style.css">
+  
+  <title>Olha p que ele fez...</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Tecno.Code</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastro</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Missão</a></li>
+            <li><a class="dropdown-item" href="#">Visão</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Valores</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-disabled="true">Saiba mais</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
-    <script src="./script/bootstrap.bundle.min.js"></script>
+<div class="box">
+  <section class="banner">
+    <div class="overlay"></div><!-- overlay-->
+    <div class="container chamada-banner">
+      <div class="row">
+          <div class="col-md-12">
+              <h2 class="text-center">Tecno.Code</h2>
+            </div><!-- col-md-12 -->
+            <div class="col-md-12">
+              <p class="text-center">Desenvolvimento do marketing digital.</p>
+            </div><!-- col-md-12 -->
+            <div class="d-grid gap-2 col-4 mx-auto">
+            <button type="button" class="btn btn-success">Success</button>
+            </div><!-- col-md-6 -->
+
+      </div><!-- row -->
+    </div><!-- container -->
+  </section>
+</div><!-- box -->
+
+<script src="./script/bootstrap.bundle.min.js"></script>
+<script src="./script/popper.min.js"></script>
 </body>
 </html>
