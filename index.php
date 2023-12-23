1bec1f4eca8f7f69e0febef77657fdcb940104c4
dbleonardo <davidbl.1281@gmail.com>
2023-12-23 08:34:24 -0300

    up: feito cards com diferencial

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta name="keywords" content="coloque, palavras, chaves, entre, virgulas, aqui">
   <link rel="stylesheet" href="./style/bootstrap.min.css">
   <link rel="stylesheet" href="./style/style.css">
+  <link rel="stylesheet" href="./style/bootstrap-icons.min.css">
   
   <title>Olha p que ele fez...</title>
 </head>
@@ -46,7 +47,7 @@
 <div class="box">
   <section class="banner">
     <div class="overlay"></div><!-- overlay-->
-    <div class="container chamada-banner">
+    <div class="container-fluid chamada-banner">
       <div class="row">
           <div class="col-md-12">
               <h2 class="text-center">Tecno.Code</h2>
@@ -62,7 +63,7 @@
   <!-- CONTEÚDO -->
 
   <section class="conteudo">
-      <div class="container">
+      <div class="container-fluid">
         <h2 class="text-center">
             Vamos buscar juntos as respostas certas!
         </h2><!-- text-center -->
@@ -77,7 +78,7 @@
 
   <!-- DADASTRO -->
   <section class="cadastro-lead">
-    <div class="container">
+    <div class="container-fluid">
       <div class="row text-center">
       <div class="fs-2 mb-3">
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-rss" viewBox="0 0 16 16">
@@ -103,7 +104,7 @@
   <!-- DEPOIMENTOS -->
 
   <section class="depoimento text-center">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
           <div class="col-md-12">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chat-square-heart-fill" viewBox="0 0 16 16">
@@ -122,44 +123,49 @@
 
   <!-- DIFERENCIAIS -->
 
-  <section class="diferencial text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-emoji-sunglasses" viewBox="0 0 16 16">
-          <path d="M4.968 9.75a.5.5 0 1 0-.866.5A4.498 4.498 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.498 3.498 0 0 1 8 11.5a3.498 3.498 0 0 1-3.032-1.75M7 5.116V5a1 1 0 0 0-1-1H3.28a1 1 0 0 0-.97 1.243l.311 1.242A2 2 0 0 0 4.561 8H5a2 2 0 0 0 1.994-1.839A2.99 2.99 0 0 1 8 6c.393 0 .74.064 1.006.161A2 2 0 0 0 11 8h.438a2 2 0 0 0 1.94-1.515l.311-1.242A1 1 0 0 0 12.72 4H10a1 1 0 0 0-1 1v.116A4.22 4.22 0 0 0 8 5c-.35 0-.69.04-1 .116"/>
-          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-1 0A7 7 0 1 0 1 8a7 7 0 0 0 14 0"/>
-        </svg>
-        <h2 class="text-center">Diferenciai#1</h2>
-        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, consectetur? Accusamus voluptatibus perspiciatis, placeat odio pariatur rerum numquam, deleniti quia sint expedita obcaecati non molestiae maiores explicabo facilis ea repudiandae?</p>
-
+  <section class="diferencial text-center" id="diferencial">
+    <div class="container ">
+    <div class="row">
+      <div class="col-sm-4 ">
+      <div class="card" style="width: 18rem;">
+        <img src="./images//bg-card-1.jpg" class="card-img-top" style="height:25rem;" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Diferencial #1</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-</svg>
-        <h2 class="text-center">Diferenciai#2</h2>
-        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, consectetur? Accusamus voluptatibus perspiciatis, placeat odio pariatur rerum numquam, deleniti quia sint expedita obcaecati non molestiae maiores explicabo facilis ea repudiandae?</p>
-
+    </div>
+      <div class="col-sm-4">
+      <div class="card" style="width: 18rem;">
+        <img src="./images//bg-card-2.jpg" class="card-img-top" style=" height:25rem;" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Diferencial #2</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         
         </div>
       </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-          <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-        </svg>
-        <h2 class="text-center">Diferenciai#3</h2>
-        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, consectetur? Accusamus voluptatibus perspiciatis, placeat odio pariatur rerum numquam, deleniti quia sint expedita obcaecati non molestiae maiores explicabo facilis ea repudiandae?</p>
-
+    </div>
+      <div class="col-sm-4">
+      <div class="card" style="width: 18rem;">
+        <img src="./images//bg-card-3.jpg" class="card-img-top" style="height:25rem;" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Diferencial #3</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         
         </div>
       </div>
-    </div><!-- container -->
+    </div>
+</div><!-- row -->
+</div><!-- container -->
   </section><!-- diferencial -->
 
+<!-- EQUIPE -->
+
+<section class="equipe">
+  <h2>
+    Equipe
+  </h2>
+</section><!-- EQUIPE -->
 
 </div><!-- box -->
 
